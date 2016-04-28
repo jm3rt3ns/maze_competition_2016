af6cdd81d6bb96cbb26a4d942261318c3644b43c
Captain Jack <Captain Jack>
2016-04-28 17:29:24 -0600

    improved movement vader

--- Maze_Competition.ino
+++ Maze_Competition.ino
@@ -7,11 +7,16 @@ int side_sw = 3;
 volatile bool front = 0;
 volatile bool side = 0;
 
+const int RIGHT_OFFSET = 3;
+const int LEFT_OFFSET = 3;
+
+int i = 0;
+
 void setup()
 {
   //set up servos
-  servo_left.attach(11);
-  servo_right.attach(12);
+  servo_left.attach(12);
+  servo_right.attach(11);
 
   // set up sensors
   pinMode(front_sw, INPUT);
@@ -24,20 +29,9 @@ void setup()
 
 void loop()
 {
-}
-
-void High()
-{
-  //run when either sensor changes
-  side = digitalRead(side_sw);
-  Serial.print("side: ");
-  Serial.println(side);
-  front = digitalRead(front_sw);
-  Serial.print("front: ");
-  Serial.println(front);
-
+  /*
   //if both low, drift left
-  if(!side&&!front)
+  /*if(!side&&!front)
   {
     servo_left.write(20);
     servo_right.write(180);
@@ -65,4 +59,54 @@ void High()
     servo_left.write(0);
     servo_right.write(0);
   }
+  */
+ 
+   //Drift_Left();
+  // Drift_Right();
+   Stop();
+  
 }
+
+
+void High()
+{
+  //run when either sensor changes
+  side = digitalRead(side_sw);
+  //Serial.print("side: ");
+  //Serial.println(side);
+  front = digitalRead(front_sw);
+  //Serial.print("front: ");
+  //Serial.println(front);
+  
+}
+
+void Drift_Left(){
+  servo_left.write(180-LEFT_OFFSET);
+  servo_right.write(20+RIGHT_OFFSET);
+}
+
+void Drift_Right(){
+  servo_left.write(110-LEFT_OFFSET);
+  servo_right.write(80+RIGHT_OFFSET);
+}
+
+void Turn_Right(){
+  Reverse();
+  delay(300);
+  servo_left.write(180-LEFT_OFFSET);
+  servo_right.write(180-RIGHT_OFFSET);
+  delay(500);
+}
+
+void Stop()
+{
+  servo_left.write(90+LEFT_OFFSET);
+  servo_right.write(90+RIGHT_OFFSET);
+}
+
+void Reverse()
+{
+  servo_left.write(0+LEFT_OFFSET);
+  servo_right.write(180-RIGHT_OFFSET);
+}
+
