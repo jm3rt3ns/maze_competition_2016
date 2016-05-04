5710e7b8bc297845aa343cc89ef4eb64cb7bde87
Captain Jack <Captain Jack>
2016-05-04 13:57:08 -0600

    implemeted with ping sensors. Hugs left wall. Readyish for competition.

--- Maze_Competition.ino
+++ Maze_Competition.ino
@@ -2,8 +2,8 @@
 Servo servo_left;
 Servo servo_right;
 
-int front_sw = 2;
-int side_sw = 3;
+int front_sw = 7;
+int side_sw = 2;
 volatile bool front = 0;
 volatile bool side = 0;
 
@@ -17,18 +17,14 @@ void setup()
   //set up servos
   servo_left.attach(12);
   servo_right.attach(11);
-
-  // set up sensors
-  pinMode(front_sw, INPUT);
-  pinMode(side_sw, INPUT);
-  attachInterrupt(digitalPinToInterrupt(front_sw), High, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(side_sw), High, CHANGE);
   
   Serial.begin(9600);
+  delay(5000);
 }
 
 void loop()
 {
+  /*if(start==1){
   //if both low, drift left
   if(!side&&!front)
   {
@@ -44,7 +40,7 @@ void loop()
   //if side low and front high, turn left
   if(!side&&front)
   {
-    Turn_Left();
+    Turn_Right();
   }
   
   //if both high, turn right
@@ -55,9 +51,67 @@ void loop()
 
   front = 0;
   side = 0;
+  }*/
+
+  long duration, inches_l,inches_f;
+  pinMode(front_sw, OUTPUT);
+  digitalWrite(front_sw,LOW);
+  delayMicroseconds(2);
+  digitalWrite(front_sw,HIGH);
+  delayMicroseconds(5);
+  digitalWrite(front_sw,LOW);
+
+  pinMode(front_sw,INPUT);
+  duration = pulseIn(front_sw,HIGH);
+
+  inches_f = duration / 74 / 2;
+
+  Serial.print("front: ");
+  Serial.println(inches_f);
+ 
+  pinMode(side_sw, OUTPUT);
+  digitalWrite(side_sw,LOW);
+  delayMicroseconds(2);
+  digitalWrite(side_sw,HIGH);
+  delayMicroseconds(5);
+  digitalWrite(side_sw,LOW);
+
+  pinMode(side_sw,INPUT);
+  duration = pulseIn(side_sw,HIGH);
+
+  inches_l = duration / 74 / 2;
+
+  Serial.print("right: ");
+  Serial.println(inches_l);
+  
+  //if left and front wall close, turn right
+  if(inches_f<=5)
+  {
+    Turn_Right();
+  }
+  //if left of control limit, correct right
+  else if(inches_l<5)
+  {
+    Correct_Right();
+  }
+  //if right of control limit, correct left
+  else if(inches_l>6&&inches_l<8)
+  {
+    Correct_Left();
+  }
+  //if left wall super far away, turn left 90 degrees and move forward a couple inches
+  else if(inches_l>7)
+  {
+    Turn_Left();
+  }
+  //otherwise if inside control limits move straight ahead
+  else
+  {
+    Forward();
+  }
 }
 
-void High()
+/*void High()
 {
   //run when either sensor changes
   side = digitalRead(side_sw);
@@ -67,9 +121,9 @@ void High()
   Serial.print("front: ");
   Serial.println(front);
   
-}
+}*/
 
-void Drift_Left(){
+/*void Drift_Left(){
   servo_left.write(180-LEFT_OFFSET);
   servo_right.write(20+RIGHT_OFFSET);
 }
@@ -81,9 +135,10 @@ void Drift_Right(){
   servo_left.write(180-LEFT_OFFSET);
   servo_right.write(180-RIGHT_OFFSET);
   delay(100);
-}
+}*/
 
-void Turn_Right(){
+void Turn_Right()
+{
   /*Reverse();
   delay(300);
   servo_left.write(180-LEFT_OFFSET);
@@ -104,14 +159,21 @@ void Turn_Right(){
   {
     side = digitalRead(side_sw);
     Drift_Left();
-  }
-  */
-  }
+  }*/
+
+  servo_left.write(180-LEFT_OFFSET);
+  servo_right.write(180-RIGHT_OFFSET);
+  delay(500);
+}
 
 void Turn_Left(){
+  Forward();
+  delay(500);
   servo_left.write(0-LEFT_OFFSET);
   servo_right.write(0-RIGHT_OFFSET);
-  delay(100);
+  delay(500);
+  Forward();
+  delay(1300);
 }
 
 void Stop()
@@ -126,3 +188,26 @@ void Reverse()
   servo_right.write(180-RIGHT_OFFSET);
 }
 
+void Correct_Left()
+{
+  servo_left.write(0-LEFT_OFFSET);
+  servo_right.write(0-RIGHT_OFFSET);
+  delay(100);
+  Forward();
+  delay(500);
+}
+
+void Correct_Right()
+{
+  servo_left.write(180-LEFT_OFFSET);
+  servo_right.write(180-RIGHT_OFFSET);
+  delay(100);
+  Forward();
+  delay(500);
+}
+
+void Forward()
+{
+  servo_left.write(180+LEFT_OFFSET);
+  servo_right.write(0-RIGHT_OFFSET);
+}
